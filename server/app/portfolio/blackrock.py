48314a22415466c79159ee3f825fdf8a63f174b6
Rahul Bhethanabotla <agentrmb@gmail.com>
2020-10-17 19:32:12 -0400

    Added methods to check performance of individual stocks

--- server/app/portfolio/blackrock.py
+++ server/app/portfolio/blackrock.py
@@ -33,10 +33,8 @@ def get_portfolio_performance_chart_data(portfolio):
     performanceData = json_data['resultMap']['PORTFOLIOS'][0]['portfolios'][0]['returns']['performanceChart']
     return performanceData
 
-# print(get_portfolio_performance_chart_data([["AAPL", 30.3], ["SNAP", 12.3]]))
-
-
-# returns how your portfolio has been done 
+# returns how your portfolio has been doing recently
+# proportional change in performance in the last month, ditto performance today, level is proportionally how much it has grown since start date, return weighted by risk over one year
 def get_latest_performance(portfolio):
     url = "https://www.blackrock.com/tools/hackathon/portfolio-analysis?apiVersion=v1&calculateExpectedReturns=true&calculateExposures=true&calculatePerformance=true&calculateRisk=true&calculateStressTests=true&endDate=20201016&includeChartData=true&positions="
     startdateurl = "&startDate=20200101"
@@ -63,7 +61,38 @@ def get_latest_performance(portfolio):
     response = requests.request('GET', url, headers=headers, data=payload)
     json_data = json.loads(response.text)
     latest_performance = json_data['resultMap']['PORTFOLIOS'][0]['portfolios'][0]['returns']['latestPerf']
-    pertinent_info = (latest_performance['oneMonth'], latest_performance['oneDay'], latest_performance['level'], latest_performance['sinceStartDateSharpeRatio'])
+    pertinent_info = (latest_performance['oneMonth'], latest_performance['oneDay'], latest_performance['level'], latest_performance['oneYearSharpeRatio'])
     return pertinent_info
 
-print(get_latest_performance([["AAPL", 30.3], ["SNAP", 12.3]]))
+
+# ticker is a string pls e.g. AAPL
+def get_performance_chart_for_one_stock(ticker):
+    url = "https://www.blackrock.com/tools/hackathon/performance?apiVersion=v1&endDate=20201016&identifiers=" + ticker + "&startDate=20200101"
+    payload = {
+        "apiVersion" :  "v1",
+        "endDate" :  "20201016",
+        "identifiers" :  ticker,
+        "startDate" :  "20200101"
+    }
+    response = requests.request("GET", url, headers={}, data=payload)
+    json_data = json.loads(response.text)
+    performance_chart = json_data['resultMap']['RETURNS'][0]['performanceChart']
+    return performance_chart
+
+
+
+# ticker is a string pls e.g. AAPL
+def get_latest_performance_for_one_stock(ticker):
+    url = "https://www.blackrock.com/tools/hackathon/performance?apiVersion=v1&endDate=20201016&identifiers=" + ticker + "&startDate=20200101"
+    payload = {
+        "apiVersion" :  "v1",
+        "endDate" :  "20201016",
+        "identifiers" :  ticker,
+        "startDate" :  "20200101"
+    }
+    response = requests.request("GET", url, headers={}, data=payload)
+    json_data = json.loads(response.text)
+    latest_performance = json_data['resultMap']['RETURNS'][0]['latestPerf']
+    pertinent_info = (latest_performance['oneMonth'], latest_performance['oneDay'], latest_performance['oneYearSharpeRatio'])
+    return pertinent_info
+
